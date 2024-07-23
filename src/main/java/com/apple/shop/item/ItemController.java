2cf2dedbd09dfb0faaa298305dadff23878ce1a4
SeungHoYoo <shmidas117@fooding.io>
2024-07-23 16:59:14 +0900

    WIP

--- src/main/java/com/apple/shop/item/ItemController.java
+++ src/main/java/com/apple/shop/item/ItemController.java
@@ -1,6 +1,7 @@
 package com.apple.shop.item;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/items")
 public class ItemController {
     @GetMapping("")
-    String list() {
+    String list(Model model) {
+        model.addAttribute("name", "홍길동");
         return "list.html";
     }
 }
