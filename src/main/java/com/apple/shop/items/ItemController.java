57830fc246ab28b3b7c864772900ef4c2c376fa0
SeungHoYoo <shmidas117@fooding.io>
2024-07-24 17:24:45 +0900

    WIP

---
+++ src/main/java/com/apple/shop/items/ItemController.java
@@ -0,0 +1,42 @@
+package com.apple.shop.items;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping("")
+    String list(Model model) {
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items", result);
+
+        return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String writePost(@ModelAttribute Item item) {
+        System.out.println(item);
+        itemRepository.save(item);
+        return "redirect:/items";
+    }
+}
